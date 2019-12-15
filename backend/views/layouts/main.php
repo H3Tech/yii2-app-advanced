1cf7e6f4579d8c5283b0730cc22f470ed1293ca1
Gyula Szabó <gt.szgyula@gmail.com>
2019-12-15 16:32:53 +0100

    Reorder some lines to conform code quality rule

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -28,13 +28,6 @@ AppAsset::register($this);
 
 <div class="wrap">
     <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
     ];
@@ -50,6 +43,13 @@ AppAsset::register($this);
             . Html::endForm()
             . '</li>';
     }
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
